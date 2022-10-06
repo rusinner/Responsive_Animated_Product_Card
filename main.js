2103e29d4d69ed7566bc4d8c03ea99814e17c22d
Theologos Mavropoulos <rusinner88@gmail.com>
2022-10-06 11:56:08 +0300

    .

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const colors = document.querySelectorAll('.color');
 const shoes = document.querySelectorAll('.shoe');
 const gradients = document.querySelectorAll('.gradient');
 const shoeBG = document.querySelector('.shoeBackground');
-let prevColor = 'blue';
+
 
 
 
@@ -17,13 +17,13 @@ function changeSize(){
 //change color on click
 function changeColor(){
 let animationEnd = true;
-
+let prevColor = 'blue';
     if (!animationEnd) return;
 let primary = this.getAttribute('primary');
 let color = this.getAttribute('color');
 let shoe = document.querySelector(`.shoe[color="${color}"]`);
 let gradient = document.querySelector(`.gradient[color="${color}"]`);
-// let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
+let prevGradient = document.querySelector(`.gradient[color="${prevColor}"]`);
 
 
     colors.forEach(c => c.classList.remove('active'));
@@ -34,10 +34,10 @@ let gradient = document.querySelector(`.gradient[color="${color}"]`);
     shoes.forEach(s => s.classList.remove('show'));
     shoe.classList.add('show');
 
-    gradients.forEach(g => g.classList.remove('first'));
-    gradient.classList.toggle('first');
-    // gradient.classList.remove('second');
-    // prevGradient.classList.add('second');
+    gradients.forEach(g => g.classList.remove('first','second'));
+    gradient.classList.add('first');
+    gradient.classList.remove('second');
+    prevGradient.classList.add('second');
 
     prevColor = color;
     animationEnd = false;
